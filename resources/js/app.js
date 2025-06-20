7ff042c40b60c467f0721f7db3d5ef9f16071f7e
Hritick Mohapatra <hritickworks@gmail.com>
2025-06-20 13:34:57 +0530

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
-// 1. ✅ Import jQuery FIRST
+
 import $ from 'jquery';
 window.$ = $;
 window.jQuery = $;
 
 // app.js
-import * as bootstrap from 'bootstrap'; // ✅ this makes it available as a variable
-window.bootstrap = bootstrap; // ✅ attach it globally if you're using it in inline scripts
+import * as bootstrap from 'bootstrap'; 
+window.bootstrap = bootstrap; 
 
-// 3. ✅ Now import your custom scripts (that use jQuery)
+
 import './ajax-crud.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
